1b025ecba42d39b6a3bea5ec9784cc2b1fb980a8
Eli Goldner <etgld@posteo.us>
2025-10-30 15:51:37 -0400

    More project draft materials

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bwrobitterman_label_studio_setup
 # lseval
+Basic version of core functionality we use with [https://github.com/bethard/anaforatools](anaforatools) but for Label Studio annotations.
---
+++ src/lseval/datatypes.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+@dataclass
+class Entity:
+    span: tuple[int, int]
+    text: str | None = None
+    cuis: set[str] = set()
+
+    def __post_init__(self):
+        if self.span[1] <= self.span[0]:
+            ValueError(f"Invalid span {self.span}")
+
+
+@dataclass
+class Relation:
+    arg1: Entity
+    arg2: Entity
+    label: Enum
+
+
+Annotation = Entity | Relation
+
+
+@dataclass
+class CorrectnessMatrix:
+    true_positives: set[Annotation] = set()
+    true_negatives: set[Annotation] = set()
+    false_positives: set[Annotation] = set()
+    false_negatives: set[Annotation] = set()
+
+    def is_true_positive(self, annotation: Annotation) -> bool:
+        return annotation in self.true_positives
+
+    def is_true_negative(self, annotation: Annotation) -> bool:
+        return annotation in self.true_negatives
+
+    def is_false_positive(self, annotation: Annotation) -> bool:
+        return annotation in self.false_positives
+
+    def is_false_negative(self, annotation: Annotation) -> bool:
+        return annotation in self.false_negatives
+
+    def get_precision(self) -> float:
+        return len(self.true_positives) / (
+            len(self.true_positives) + len(self.false_positives)
+        )
+
+    def get_recall(self) -> float:
+        return len(self.true_positives) / (
+            len(self.true_positives) + len(self.false_negatives)
+        )
+
+    def get_f_beta(self, beta: float) -> float:
+        precision = self.get_precision()
+        recall = self.get_recall()
+        beta_squared = pow(beta, 2.0)
+        return ((1 + beta_squared) * precision * recall) / (
+            (beta_squared * precision) + recall
+        )
+
+    def get_f1(self) -> float:
+        return self.get_f_beta(beta=1.0)
