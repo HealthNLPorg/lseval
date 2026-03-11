f8784bdef1e76ae3b29f20fa35582c9aaecd0515
Eli Goldner <etgld@posteo.us>
2026-03-11 14:56:10 -0400

    Less confusion condition

--- src/lseval/score.py
+++ src/lseval/score.py
@@ -82,9 +82,8 @@ def overlap_entity_correctness_matrix(
         else:
             false_positive_entities.update(entities)
     for span, entities in reference_span_to_entities.items():
-        if any(overlap_match(span, pred_span) for pred_span in sorted_predicted_spans):
-            continue
-        false_negative_entities.update(entities)
+        if not any(overlap_match(span, pred_span) for pred_span in sorted_predicted_spans):
+            false_negative_entities.update(entities)
 
     return CorrectnessMatrix(
         true_positives=true_positive_entities,
